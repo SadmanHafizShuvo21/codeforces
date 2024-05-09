1aaf8fb6905a494a15055a93b6195626276359e6
SadmanHafizShuvo21 <ug2102021@cse.pstu.ac.bd>
2024-05-09 19:55:00 +0600

    A_Watermelon

---
+++ A_Watermelon.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n>2 && n%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
+int main(){
+    solve();
+}
