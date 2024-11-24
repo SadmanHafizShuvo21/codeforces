5a64a0cd14ea1feb666168f56e4668ba5c7ee7b9
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2024-11-24 22:05:20 +0600

    codeforces

---
+++ codeforce/A_Shohag_Loves_Mod.cpp
@@ -0,0 +1,21 @@
+//24 Nov 2024
+#include<bits/stdc++.h>
+using ll = long long;
+void solve(){
+    int n,i=1;
+    std::cin>>n;
+    while(n--){
+        std::cout<<i<<" ";
+        i+=2;
+    }
+    std::cout<<"\n";
+}
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin>>t;
+    while(t--)
+    solve();
+}
+
