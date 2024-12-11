1ee0dfb13305c73b8037519bcbfc772904226725
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2024-12-11 19:42:54 +0600

    codeforces

--- codeforce/E_Control_of_Randomness.cpp
+++ codeforce/E_Control_of_Randomness.cpp
@@ -1,9 +1,10 @@
-//10 Dec 2024
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using ll = long long;
-void solve(){
-    int n,q;
-    std::cin>>n>>q;
+
+void solve() {
+    int n, q;
+    std::cin >> n >> q;
+
     std::vector<std::vector<int>> adj(n);
     for (int i = 1; i < n; i++) {
         int u, v;
@@ -13,30 +14,38 @@ void solve(){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    std::vector<int> p(n, -1), dep(n);
-    auto dfs = [&](this auto &&self, int x) -> void {
+
+    std::vector<int> p(n, -1), dep(n, 0);
+
+    // dfs lambda function
+    std::function<void(int)> dfs = [&](int x) {
         for (auto y : adj[x]) {
             if (y == p[x]) {
                 continue;
             }
             p[y] = x;
             dep[y] = dep[x] + 1;
-            self(y);
+            dfs(y);
         }
     };
-    dfs(0);
-    
+
+    dfs(0); // Start DFS from the root (node 0)
+
     for (int i = 0; i < q; i++) {
         int v, c;
         std::cin >> v >> c;
         v--;
-        
+
         int ans = dep[v];
-        std::vector<int> cnt(n);
+        std::vector<int> cnt(n, 0);
+
+        // Traverse parent chain
         for (int x = p[v]; x > 0; x = p[p[x]]) {
             cnt[adj[x].size()]++;
             ans += 2 * (adj[x].size() - 1);
         }
+
+        // Optimize by reducing `c`
         for (int d = n - 1; d > 0; d--) {
             while (cnt[d] > 0 && c > 0) {
                 cnt[d]--;
@@ -44,18 +53,17 @@ void solve(){
                 ans -= 2 * (d - 1);
             }
         }
+
         std::cout << ans << "\n";
     }
-
 }
-int main(){
+
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
+
     int t;
-    std::cin>>t;
-    while(t--)
+    std::cin >> t;
+    while (t--) 
     solve();
 }
-
-
-
