9f1be3ee8adbdd9f2f61e3d88dc40705644b6ff6
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2024-12-25 21:16:17 +0600

    codeforces

--- B_Digits.cpp
+++ B_Digits.cpp
@@ -2,9 +2,37 @@
 #include<bits/stdc++.h>
 using ll = long long;
 void solve(){
-    int n,d;
-    std::cin>>n>>d;
-    
+    int n, d;
+    std::cin >> n >> d;
+    std::vector<int>v;
+    v.push_back(1);
+    if(d%3==0 || n>=3){
+        v.push_back(3);
+    }
+    if(d==5){
+        v.push_back(5);
+    }
+    if(d==7 || n>=3){
+        v.push_back(7);
+    }
+    if(d % 9 == 0){
+        v.push_back(9);
+    } 
+    else if(d % 3 == 0) {
+        if(n >= 3){
+            v.push_back(9);
+        } 
+    }
+    else{
+        if(n >= 6){
+            v.push_back(9);
+        } 
+    }
+
+    for(int i : v){
+        std::cout<<i<<" ";
+    }
+    std::cout<<"\n";
 }
 int main(){
     std::ios::sync_with_stdio(false);
