a994b91b9752346788e480fd502f0768601647aa
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-02-09 01:04:50 +0600

    codeforces

--- F_We_Were_Both_Children.cpp
+++ F_We_Were_Both_Children.cpp
@@ -1,42 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-
-        // Frequency map for hop lengths
-        unordered_map<int, int> freq;
-        for (int x : a) {
-            freq[x]++;
-        }
-
-        // Count the number of frogs for each x
-        vector<int> count(n + 1, 0);
-        for (auto& [d, cnt] : freq) {
-            for (int x = d; x <= n; x += d) {
-                count[x] += cnt;
-            }
-        }
-
-        // Find the maximum count
-        int maxFrogs = 0;
-        for (int x = 1; x <= n; x++) {
-            maxFrogs = max(maxFrogs, count[x]);
-        }
-
-        cout << maxFrogs << "\n";
+#include<bits/stdc++.h>
+using ll = long long int ;
+ 
+void solve(){
+    ll n;
+    std::cin >> n ;
+    std::vector<ll>a(n);
+    for(ll i=0;i<n;i++){
+        std::cin>>a[i];
     }
-
-    return 0;
+    std::map<ll,ll>mp;
+    for(ll x : a){
+        mp[x]++;
+    }
+    std::vector<ll> count(n + 1, 0);
+    for (auto& [key, cnt] : mp) {
+        for (int i = key; i <= n; i += key) {
+            count[i] += cnt;
+        }
+    }
+    std::cout << *max_element(count.begin(),count.end()) << "\n";
+}
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin>>t;
+    while(t--){
+        solve();
+    }
 }
