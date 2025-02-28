a11dcd7804172f8a05a3ddf7fbcc15894633c21d
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-03-01 00:18:44 +0600

    codeforces

---
+++ A_The_Play_Never_Ends.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    if(n%3==1){
+        std::cout<<"YES"<<"\n";
+    }
+    else{
+        std::cout<<"NO"<<"\n";
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ B_Perfecto.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+bool isPerfectSquare(ll x) {
+    ll s = std::sqrt(x);
+    return (1LL * s * s == x);
+}
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    
+    ll sum = n*(n+1)/2;
+    if(isPerfectSquare(sum)){
+        std::cout<<-1<<'\n';
+        return;
+    }
+    std::vector<ll>a(n);
+    std::iota(a.begin(),a.end(),1);// store data 1 to n
+    for(int i=0;i<n;i++){
+        std::cout<<a[i]<<" \n"[i==n-1];
+    }
+    for (int i = 1; i < n; i++) {
+        if (isPerfectSquare(1LL * i * (i + 1) / 2)) {
+            std::swap(a[i], a[i - 1]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        std::cout<<a[i]<<" \n"[i==n-1];
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+}
