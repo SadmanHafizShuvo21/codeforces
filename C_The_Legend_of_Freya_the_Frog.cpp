6cc6b633fc38414ae0321dd4657d382dc1b71602
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-03-14 21:46:35 +0600

    codeforces

--- C_The_Legend_of_Freya_the_Frog.cpp
+++ C_The_Legend_of_Freya_the_Frog.cpp
@@ -4,7 +4,9 @@ using ll = long long;
 void solve(){
     ll x,y,k;
     std::cin>>x>>y>>k;
-    std::cout<<std::max(2LL*((x+k-1)/k)-1,2LL*(y+k-1)/k)<<"\n";
+    x=(x+k-1)/k;
+    y=(y+k-1)/k;
+    std::cout<<std::max(2LL*x-1,2LL*y)<<"\n";
 }
 int main() {
     std::ios::sync_with_stdio(false);
