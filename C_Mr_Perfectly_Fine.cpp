4227f8e1f4952d83a3a3eb08342ffef2bd78a762
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-03-24 19:49:19 +0600

    codeforces

---
+++ C_Mr_Perfectly_Fine.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using ll = long long;
+const ll inf = 10E9+7;
+
+void solve(){
+    ll n,k;
+    std::cin>>n;
+    std::vector<ll>pref(4,inf);
+    ll ans = inf;
+    while(n--){
+        ll x;
+        std::string s;
+        std::cin>>x>>s;
+        ll t = (s[0]-'0')|(s[1]-'0')<<1;
+        pref[t]=std::min(pref[t],x);
+    }
+    ans = std::min(pref[3],pref[1]+pref[2]);
+    if(ans>=inf){
+        std::cout<<-1<<"\n";
+    }
+    else{
+        std::cout<<ans<<'\n';
+    }
+    
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int testcase;
+    std::cin>>testcase;
+    while(testcase--){
+        solve();
+    }
+}
