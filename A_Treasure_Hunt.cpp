4c2d272762a3dbcf370a733739da7debd21c9974
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-03-28 11:31:41 +0600

    codeforces

---
+++ A_Treasure_Hunt.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+void solve(){
+    ll x, y, a;
+    std::cin >> x >> y >> a;
+    double half = a + 0.5;
+    ll full_cycles = a / (x + y);
+    double remaining = half - full_cycles * (x + y);
+    if (remaining <= x){
+        std::cout << "NO" << "\n";
+    }
+    else{
+        std::cout << "YES" << "\n";
+    }
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+    while (t--){
+        solve();
+    }
+}
