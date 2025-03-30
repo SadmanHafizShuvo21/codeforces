62d9b05046849d3a959a172d9590a2b11575dec4
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-03-30 11:23:51 +0600

    codeforces

--- B_Shohag_Loves_Strings.cpp
+++ B_Shohag_Loves_Strings.cpp
@@ -5,18 +5,19 @@ void solve(){
     std::string s;
     std::cin>>s;
     ll n=s.size();
-    if(n==1){
-        std::cout<<-1<<"\n";
-    }
     for(int i=0;i<n-1;i++){
         if(s[i]==s[i+1]){
             std::cout<<s.substr(i,2)<<"\n";
             return;
         }
     }
-    if(n>=3){
-        std::cout<<s.substr(0,3)<<"\n";
+    for(int i=0;i<n-2;i++){
+        if(s[i]!=s[i+2]){
+            std::cout<<s.substr(i,3)<<"\n";
+            return;
+        }
     }
+    std::cout<<-1<<"\n";
 }
 
 int main(){
@@ -28,37 +29,3 @@ int main(){
         solve();
     }
 }
-// #include <iostream>
-// #include <string>
-// using namespace std;
-
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     int t;
-//     cin >> t;
-//     while (t--) {
-//         string s;
-//         cin >> s;
-//         int n = s.size();
-//         if (n == 1) {
-//             cout << "-1\n";
-//             continue;
-//         }
-//         bool found = false;
-//         for (int i = 0; i < n - 1; ++i) {
-//             if (s[i] == s[i+1]) {
-//                 cout << s.substr(i, 2) << '\n';
-//                 found = true;
-//                 break;
-//             }
-//         }
-//         if (found) continue;
-//         if (n >= 3) {
-//             cout << s.substr(0, 3) << '\n';
-//         } else {
-//             cout << "-1\n";
-//         }
-//     }
-//     return 0;
-// }
