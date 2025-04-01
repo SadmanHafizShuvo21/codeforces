4292c9698a31eb094674977afb36096b57b27b1b
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-01 19:19:21 +0600

    codeforces

--- B_Lady_Bug.cpp
+++ B_Lady_Bug.cpp
@@ -2,44 +2,32 @@
 using ll = long long;
 
 void solve() {
-    int n;
+    ll n;
     std::cin >> n;
     std::string a, b;
     std::cin >> a >> b;
-    
-    int A = 0, x = 0;
-    int B = 0, y = 0;
-
-    for (int i = 0; i < n; i++) {
-        if ((i % 2) == 0) { 
-            A++;
-            if (a[i] == '0'){
+    ll x=0,y=0;
+    for(int i=0;i<n;i++){
+        if(a[i]=='0'){
+            if(i%2==0){
                 x++;
-            } 
+            }
+            else{
+                y++;
+            }
+        }
+        
+        if(b[i]=='0'){
+            if((i+1)%2==0){
+                x++;
+            }  
+            else{
+                y++;
+            }
         }
     }
-    for (int i = 1; i < n; i += 2) { 
-        if (b[i] == '0'){
-            x++;
-        }
-    }
-
-    for (int i = 1; i < n; i += 2) { 
-        B++;
-        if (a[i] == '0'){
-            y++;
-        } 
-    }
-    for (int i = 0; i < n; i += 2) { 
-        if (b[i] == '0'){
-            y++;
-        } 
-    }
-    
-    bool ok1 = (x >= A);
-    bool ok2 = (y >= B);
-    
-    std::cout << (ok1 && ok2 ? "YES" : "NO") << "\n";
+    // std::cout<<x<<" "<<y<<"\n";
+    std::cout<<(x>=(n+1)/2 && y>=n/2 ? "YES":"NO")<<"\n";
 }
 
 int main() {
