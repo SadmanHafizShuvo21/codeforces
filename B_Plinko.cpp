bb20d3c3d6f358f12d753d5fffdbc634b68d090b
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-02 19:09:12 +0600

    codeforces

---
+++ B_Plinko.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+void solve() {
+    int x;
+    std::string s;
+    std::cin>>s>>x;
+    std::cout<<17<<"\n"; 
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    solve();
+}
