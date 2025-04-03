416156d4a4d8feba7319127c8f153334f20a16d3
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-03 22:58:41 +0600

    codeforces

--- C_Would_It_Be_Unrated.cpp
+++ C_Would_It_Be_Unrated.cpp
@@ -2,15 +2,18 @@
 using ll = long long;
 
 void solve() {
-    int x;
-    std::string s;
-    getline(std::cin,s);
-    std::cout<<143<<"\n"; 
+    ll n;
+    std::cin>>n;
+    std::cout<<2*n<<"\n";
+
 }
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     
+    int t;
+    std::cin>>t;
+    while(t--)
     solve();
 }
