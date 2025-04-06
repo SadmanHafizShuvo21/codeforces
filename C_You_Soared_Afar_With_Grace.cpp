950ac5bf17a4cb74068fb022e65657a1203a8346
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-06 10:42:07 +0600

    codeforces

--- C_You_Soared_Afar_With_Grace.cpp
+++ C_You_Soared_Afar_With_Grace.cpp
@@ -5,45 +5,53 @@ void solve() {
     int n;
     std::cin >> n;
     
-    std::vector<int> a(n), b(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> a[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        std::cin >> b[i];
-    }
-
-    std::vector<std::pair<int, int>> operations;
-    bool possible = true;
-
-    for (int i = 0; i < n; ++i) {
-        if (a[i] != b[n - 1 - i]) {
-            bool found = false;
-            for (int j = i + 1; j < n; ++j) {
-                if (a[j] == b[n - 1 - i]) {
-                    std::swap(a[i], a[j]);
-                    std::swap(b[i], b[j]);
-                    operations.push_back({i + 1, j + 1});
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                possible = false;
-                break;
-            }
-        }
-    }
+    std::vector<int> a(n), b(n), p(n), ia(n);
     
-    if (possible) {
-        std::cout << operations.size() << '\n';
-        for (const auto &op : operations) {
-            std::cout << op.first << ' ' << op.second << '\n';
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+        a[i]--;
+        ia[a[i]] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+        b[i]--;
+        p[a[i]] = b[i];
+    }
+
+    // Count invalid mappings (more than one improper cycle)
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (p[i] == i || p[p[i]] != i) {
+            cnt++;
         }
-    } 
-    else {
-        std::cout << -1 << '\n';
+    }
+
+    if (cnt > 1) {
+        std::cout << -1 << "\n";
+        return;
+    }
+
+    std::vector<int> q(n);
+    for (int i = 0, l = 0, r = n - 1; i < n; i++) {
+        if (p[i] == i) {
+            q[ia[i]] = n / 2;
+        } else if (p[i] > i) {
+            q[ia[i]] = l++;
+            q[ia[p[i]]] = r--;
+        }
+    }
+
+    std::vector<std::array<int, 2>> ans;
+    for (int i = 0; i < n; i++) {
+        while (q[i] != i) {
+            ans.push_back({i, q[i]});
+            std::swap(q[i], q[q[i]]);
+        }
+    }
+
+    std::cout << ans.size() << "\n";
+    for (auto [x, y] : ans) {
+        std::cout << x + 1 << " " << y + 1 << "\n";
     }
 }
 
@@ -56,4 +64,6 @@ int main() {
     while (t--) {
         solve();
     }
+
+    return 0;
 }
