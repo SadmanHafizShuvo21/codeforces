23fe65a1d99bb69b3d55e3a04696d2e1f66bb5e7
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-09 09:56:34 +0600

    codeforces

--- C_Simple_Repetition.cpp
+++ C_Simple_Repetition.cpp
@@ -2,11 +2,19 @@
 using ll = long long;
 
 bool is_prime(ll n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
     for (ll d = 3; d * d <= n; d += 2) {
-        if (n % d == 0) return false;
+        if (n % d == 0) {
+            return false;
+        }
     }
     return true;
 }
@@ -15,14 +23,14 @@ void solve() {
     ll x, k;
     std::cin >> x >> k;
     if (k == 1) {
-        std::cout << (is_prime(x) ? "YES\n" : "NO\n");
+        std::cout << (is_prime(x) ? "YES" : "NO") << "\n";
     } 
     else {
         if (x == 1 && k == 2) {
-            std::cout << "YES\n";
+            std::cout << "YES" << "\n";
         } 
         else {
-            std::cout << "NO\n";
+            std::cout << "NO" << "\n";
         }
     }
 }
