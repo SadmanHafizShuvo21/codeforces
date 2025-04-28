8f532659e1fdbaa565711d97f3ea4faa7b408e4c
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-04-28 22:39:56 +0600

    codeforces

--- C_Card_Game.cpp
+++ C_Card_Game.cpp
@@ -1,98 +1,26 @@
 #include <bits/stdc++.h>
 using ll = long long;
 
-bool ok(int a, int b, int n) {
-    if (a == 1 && b == n) {
-        return true;
-    }
-    if (b == 1 && a == n) {
-        return false;
-    }
-    return a > b;
-}
-
-bool beat(std::vector<int>& p, std::vector<int>& o, int n) {
-    for (int c : p) {
-        bool win = true;
-        for (int oc : o) {
-            if (!ok(c, oc, n)) {
-                win = false;
-                break;
-            }
-        }
-        if (win) {
-            return true;
-        }
-    }
-    return false;
-}
-
 void solve() {
     int n;
     std::cin >> n;
+    
     std::string s;
     std::cin >> s;
     
-    std::vector<int> a, b;
-    for (int i = 0; i < n; i++) {
-        if (s[i] == 'A') {
-            a.push_back(i + 1);
-        } else {
-            b.push_back(i + 1);
-        }
-    }
-    
-    if (beat(a, b, n)) {
-        std::cout << "Alice\n";
-        return;
-    }
-    if (beat(b, a, n)) {
-        std::cout << "Bob\n";
-        return;
-    }
-
-    int maxA = *std::max_element(a.begin(), a.end());
-    int maxB = *std::max_element(b.begin(), b.end());
-
-    bool bob = false;
-    for (int x : b) {
-        if (ok(x, maxA, n)) {
-            bob = true;
-            break;
-        }
-    }
-    
-    if (bob) {
-        std::cout << "Bob\n";
-        return;
-    }
-
-    bool alice = false;
-    for (int x : a) {
-        if (ok(x, maxB, n)) {
-            alice = true;
-            break;
-        }
-    }
-
-    if (alice) {
-        std::cout << "Alice\n";
-        return;
-    }
-
-    if (maxA > maxB) {
-        std::cout << "Alice\n";
-    } 
-    else if (maxB > maxA) {
-        std::cout << "Bob\n";
-    } 
-    else {
-        if (a.size() > b.size()) {
+    if (s[n - 1] == 'B') {
+        if (std::count(s.begin(), s.end(), 'B') == 1) {
             std::cout << "Alice\n";
-        } 
-        else {
+        } else {
             std::cout << "Bob\n";
         }
+        return;
+    }
+    
+    if (s[0] == 'A' || s[n - 2] == 'A') {
+        std::cout << "Alice\n";
+    } else {
+        std::cout << "Bob\n";
     }
 }
 
