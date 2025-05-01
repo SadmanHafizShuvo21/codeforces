2b6437647bb42f7034405d88ba46ba775ae405ec
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-05-01 12:28:50 +0600

    codeforces

---
+++ C_Creating_Keys_for_StORages_Has_Become_My_Main_Skill.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+void solve() {
+    ll n, x;
+    std::cin >> n >> x;
+    std::vector<ll> a(n);
+    ll cur = 0;
+    ll val = 0;
+
+    while (cur < n && ((cur & x) == cur)) {
+        val |= cur;
+        cur++;
+    }
+
+    if(cur == n && val != x) {
+        cur--;
+    }
+    for (int i = 0; i < n; i++) {
+        std::cout << (i < cur ? i : x) << " \n"[i + 1 == n];
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+    while (t--) solve();
+}
