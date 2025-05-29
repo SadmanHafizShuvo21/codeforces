88af3e5f909100492ad92c8c166ec60be3e873e4
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-05-29 23:15:16 +0530

    codeforce

--- B_Maximum_Multiple_Sum.cpp
+++ B_Maximum_Multiple_Sum.cpp
@@ -1,22 +1,16 @@
-//13 Jun 2024
 #include<bits/stdc++.h>
 using ll = long long;
 void solve(){
     ll n;
     std::cin >> n; 
-    if(n==3){
-        std::cout<<3<<"\n";
-    }
-    else{
-        std::cout<<2<<"\n";
-    }
+    std::cout << (n == 3 ? 3 : 2) << "\n";
 }
 int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    int tesecase;
-    std::cin>>tesecase;
-    while(tesecase--)
+    int t;
+    std::cin>>t;
+    while(t--)
     solve();
 }
 
