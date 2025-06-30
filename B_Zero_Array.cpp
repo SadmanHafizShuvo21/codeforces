4f7219c356d624644dc2f4fc5233da41f1f0b99e
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-06-30 20:43:31 +0600

    codeforces

---
+++ B_Zero_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using ll = long long;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    ll sum = std::accumulate(a.begin(), a.end(), 0LL);
+    ll mx = *std::max_element(a.begin(), a.end());
+
+    std::cout << (sum %2 == 0 && sum >= 2 * mx ? "YES" : "NO") << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);  
+    solve();
+}
