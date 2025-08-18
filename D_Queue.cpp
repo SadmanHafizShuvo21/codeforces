cfcb8b942538ca2250b6b9f7fd748fd0a33b137c
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-08-19 02:04:05 +0600

    codeforces

---
+++ D_Queue.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using ll = long long;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll>a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    ll ans = 0, sum = 0;
+    std::sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++) {
+        if (sum <= a[i]) {
+            sum += a[i];
+            ans++;
+        }
+    }
+    std::cout << ans << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    solve();
+}
