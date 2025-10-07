9c559aeb1b58c3dcea48fb2257dfda7284b1666d
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-10-07 16:03:23 +0600

    codeforces

---
+++ B_Deck_of_Cards.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using ll = long long;
+const ll N = 2e5 + 5;
+
+void solve() {
+    ll n, k;
+    std::cin >> n >> k;
+    std::string s;
+    std::cin >> s;
+    std::map<ll, ll> mp;
+    for (char ch : s) {
+        mp[ch - '0']++;
+    }
+
+    std::string str(n, 0);
+    for (int i = 0; i < n; i++) {
+        if (i < mp[0] || i >= n - mp[1] || n == k) {
+            str[i] = '-';
+        } 
+        else if (i >= mp[0] + mp[2] && i < n - mp[1] - mp[2]) {
+            str[i] = '+';
+        } 
+        else{
+            str[i] = '?';
+        }
+    }
+    std::cout << str << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    while(t--) {
+        solve();
+    }
+}
