b1d5acf813bf5a2b1e77b31ed0abb6999e32fb61
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-10-19 09:34:14 +0600

    codeforces

--- D_Beautiful_Permutation.cpp
+++ D_Beautiful_Permutation.cpp
@@ -3,7 +3,7 @@ using ll = long long;
 const ll N = 2e5 + 5;
     
 ll ask (ll a, ll l, ll r) {
-    std::cout << a << ' ' << l  << ' ' << r << '\n' << std::flush;;
+    std::cout << a << ' ' << l << ' ' << r << '\n' << std::flush;
     ll x;
     std::cin >> x;
     return x;
@@ -12,13 +12,13 @@ void solve() {
     ll n;
     std::cin >> n;
     
-    ll m = ask(2,1, n) - ask(1, 1, n);
+    ll m = ask(2, 1, n) - ask(1, 1, n);
     ll l = 1, r = n, mid, ans = 0;  
     while (l <= r) {
         mid = (l + r) / 2;
         ll a = ask(1, 1, mid), b = ask(2, 1, mid);
         if (a == b) {
-            ans = mid;
+            ans = std::max(ans, mid);
             l = mid + 1;
         }
         else {
