7fad437082754c9228cd02ad49e29fb1265bda44
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-10-27 12:39:32 +0600

    codeforces gym

--- C_Crochet_Competition.cpp
+++ C_Crochet_Competition.cpp
@@ -1,95 +1,62 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define dbg(x) cout << #x << " = " << x << '\n';
+#include<bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
 
-void solve()
-{
-    vector<string> day = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",
-                          "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    string dl, dr;
-    int hl, ml, hr, mr, dpos;
-    char ch;
-    cin >> dl >> hl >> ch >> ml >> dr >> hr >> ch >> mr;
-    for (int i = 0;; i++)
-        if (day[i] == dl)
-        {
-            dpos = i;
-            break;
-        }
-    int ans = 0;
-    while (true)
-    {
-        if (day[dpos] == dr && hl == hr && ml == mr)
-            break;
-        ans++;
-        ml++;
-        if (ml == 60)
-            hl++, ml = 0;
-        if (hl == 24)
-            dpos++, hl = 0;
-    }
-    if (ans == 0)
-    {
-        cout << "7 days\n";
-        return;
+void solve() {
+    std::map<std::string, ll> v = {
+        {"Mon", 1}, {"Tue", 2}, {"Wed", 3},
+        {"Thu", 4}, {"Fri", 5}, {"Sat", 6}, {"Sun", 7}
+    }; 
+    std::string a, b, tf, ts;
+    std::cin >> a >> tf;
+    std::cin >> b >> ts;
+
+    ll fh, fm, sh, sm;
+    fh = std::stoll(tf.substr(0, 2)), fm = std::stoll(tf.substr(3, 2)), sh = std::stoll(ts.substr(0, 2)), sm = std::stoll(ts.substr(3, 2));
+    // std::cout << a << " " << fh << ' ' << fm << "\n";
+    // std::cout << b << " " << sh << ' ' << sm << "\n";
+
+    ll day = (v[b]  - v[a]) * 24 * 60;
+    ll min = (sh * 60 + sm) - (fh * 60 + fm);
+
+    ll total = day + min;
+    if (total <= 0) {
+        total += 7 * 24 * 60;
     }
 
-    int d = ans / (24 * 60);
-    ans %= (24 * 60);
-    if (ans == 0)
-    {
-        cout << d;
-        if (d > 1)
-            cout << " days\n";
-        else
-            cout << " day\n";
-        return;
-    }
-    int h = ans / 60;
-    ans %= 60;
-    if (ans == 0)
-    {
-        if (d)
-        {
-            cout << d;
-            if (d > 1)
-                cout << " days and ";
-            else
-                cout << " day and ";
-        }
-        cout << h;
-        if (h > 1)
-            cout << " hours\n";
-        else
-            cout << " hour\n";
-        return;
+    // std::cout << total << "\n";
+
+    ll days = total / (24 * 60);
+    total %= (24 * 60);
+    ll hours = total / 60;
+    ll minutes = total % 60;
+
+    std::vector<std::string> parts;
+    if (days) {
+        parts.push_back(std::to_string(days) + " day" + (days > 1 ? "s" : ""));
     }
 
-    if (d)
-    {
-        cout << d;
-        if (d > 1)
-            cout << " days, ";
-        else
-            cout << " day, ";
+    if (hours) {
+        parts.push_back(std::to_string(hours) + " hour" + (hours > 1 ? "s" : ""));
     }
-    if (h)
-    {
-        cout << h;
-        if (h > 1)
-            cout << " hours, ";
-        else
-            cout << " hour, ";
+
+    if (minutes) {
+        parts.push_back(std::to_string(minutes) + " minute" + (minutes > 1 ? "s" : ""));
     }
-    cout << ans;
-    if (ans > 1)
-        cout << " minutes\n";
-    else
-        cout << " minute\n";
+
+    if (parts.size() == 1) {
+        std::cout << parts[0] << '\n';
+    }
+    else if (parts.size() == 2) {
+        std::cout << parts[0] << " and " << parts[1] << '\n';
+    }
+    else if (parts.size() == 3) {
+        std::cout << parts[0] << ", " << parts[1] << ", " << parts[2] << '\n';
+    }
 }
 
-int main()
-{
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
