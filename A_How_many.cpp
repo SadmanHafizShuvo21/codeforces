c7f1c65b9049065595555061384a622c8c1b093d
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-11-10 23:05:21 +0600

    codeforces

---
+++ A_How_many.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+
+    std::cout << 3 * n << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    solve();
+    // int t;
+    // std::cin >> t;
+    // while(t--) {
+    //     solve();
+    // }
+}
---
+++ C_XOR_LCM.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
+void solve() {
+    ll n;
+    std::cin >> n;
+    ll x = 1;
+    while (x <= n) {
+        x <<= 1;
+    }
+    std::cout << n << " " << n * x << '\n';
+}
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+}
