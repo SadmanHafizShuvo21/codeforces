cc4fc4c72236901daa6137e211d7277053b5fe67
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-12-01 22:40:34 +0600

    codeforces

---
+++ B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    
+    ll ans = 1;
+    while(n % (ans + 1) == 0) {
+        ans++;
+    }
+    std::cout << ans << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while(t--) {
+        solve();
+    }
+    // solve();
+}
