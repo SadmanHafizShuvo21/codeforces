ce9967ac312811040a0913ca2fee5a980f5435aa
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-12-10 19:16:54 +0600

    codeforces

---
+++ A_Min_Max_Swap.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+        if (b[i] > a[i]) {
+            std::swap(a[i], b[i]);
+        }
+    }
+
+    std::cout << *std::max_element(a.begin(), a.end()) * *std::max_element(b.begin(), b.end()) << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while(t--) {
+        solve();
+    }
+    // solve();
+}
