169f19ff28a5a96e98a18ebe42fa15f498fc263b
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2025-12-14 18:41:14 +0600

    codeforces

---
+++ I_Peak_Reduction.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    std::cout << (((a[0] == 2 && a[n - 1] == 1) || (a[0] == 1 && a[n - 1] == 2)) ? "YES" : "NO") << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while(t--) {
+        solve();
+    }
+    // solve();
+}
