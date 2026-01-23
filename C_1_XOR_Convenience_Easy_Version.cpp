c6a552226afcbe09ad1ba190f85a37fa58f78377
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-01-24 00:04:40 +0600

    codeforces

---
+++ C_1_XOR_Convenience_Easy_Version.cpp
@@ -0,0 +1,36 @@
+// 24 Jan 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n + 1);
+
+    a[1] = (n % 2 ? n - 1 : n);
+    for (int i = 2; i < n; i++) {
+        a[i] = i ^ 1;
+    }
+    a[n] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        std::cout << a[i] << " \n"[i == n];
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    // solve();
+}
