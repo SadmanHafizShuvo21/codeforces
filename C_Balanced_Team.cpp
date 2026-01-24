7c445e2bcb05bb598b79c1ff630c058179682269
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-01-24 19:45:01 +0600

    codeforces

---
+++ C_Balanced_Team.cpp
@@ -0,0 +1,40 @@
+// 24 Jan 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    std::sort(a.begin(), a.end());
+
+    ll ans = 1, j = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] - a[j] > 5) {
+            j++;
+        }
+        ans = std::max(ans, i - j + 1);
+    }
+
+    std::cout << ans << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    // int t;
+    // std::cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+    solve();
+}
