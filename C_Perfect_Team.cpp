bd6112f876faa17fe755322e9aea49344d00aa76
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-02-12 19:41:04 +0600

    codeforces

---
+++ C_Perfect_Team.cpp
@@ -0,0 +1,27 @@
+// 11 Feb 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll c, m, x;
+    std::cin >> c >> m >> x;
+    std::cout << std::min({c, m, (c + m + x) / 3}) << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    // solve();
+}
+
