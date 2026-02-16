c5a87f90d93c44682f62be51aca3112733266d4d
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-02-16 23:56:10 +0600

    codeforces

--- C_Dice_Roll_Sequence.cpp
+++ C_Dice_Roll_Sequence.cpp
@@ -10,38 +10,17 @@ const ll error = 1e-6;
 void solve() {
     ll n;
     std::cin >> n;
-    std::vector<ll> a(n + 1, 0);
-    for (int i = 1; i <= n; i++) {
+    std::vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
         std::cin >> a[i];
     }
-    
-    std::map<ll, ll> mp;
-    mp[1] = 6;
-    mp[2] = 5;
-    mp[3] = 4;
-    mp[4] = 3;
-    mp[5] = 2;
-    mp[6] = 1;
 
-    std::vector<std::vector<ll>> dp(n + 1, std::vector<ll>(7, inf));
-    for (int v = 1; v <= 6; v++) {
-        dp[1][v] = (a[1] != v);
-    }
-
-    for (int i = 2; i <= n; i++) {
-        for (int v = 1; v <= 6; v++) {
-            for (int u = 1; u <= 6; u++) {
-                if (v == u || v == mp[u]) {
-                    continue;
-                }
-                dp[i][v] = std::min(dp[i][v], dp[i - 1][u] + (a[i] != v));
-            }
+    ll ans = 0;
+    for (int i = 0; i <n - 1; i++) {
+        if (a[i + 1] == 7 - a[i] || a[i] == a[i + 1]) {
+            ans++;
+            i++;
         }
-    }
-
-    ll ans = inf;
-    for (int i = 1; i <= 6; i++) {
-        ans = std::min(ans, dp[n][i]);
     }
 
     std::cout << ans << '\n';
