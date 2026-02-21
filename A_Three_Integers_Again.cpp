882e9242afaca875697bd1a629fc152dfc6c2b4a
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-02-22 00:14:17 +0600

    Kotlin

---
+++ A_Three_Integers_Again.cpp
@@ -0,0 +1,26 @@
+// 21 Feb 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n, m;
+    std::cin >> n >> m;
+    std::cout << n - (m - 1) << " " << 1 << " " << m - 1 << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    // solve();
+}
---
+++ B_Bad_Days.cpp
@@ -0,0 +1,45 @@
+// 21 Feb 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+    std::vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    ll x = -inf, y = -inf, ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < y) {
+            ans++;
+        }
+
+        if (a[i] > x) {
+            y = x;
+            x = a[i];
+        } 
+        else if (a[i] > y) {
+            y = a[i];
+        }
+    }
+    std::cout << ans << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    // int t;
+    // std::cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+    solve();
+}
