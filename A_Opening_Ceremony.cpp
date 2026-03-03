92c114332fc3f6dbef18a2ca6fc77b600df7da8b
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-03-03 22:36:53 +0600

    codeforces

--- A_Opening_Ceremony.cpp
+++ A_Opening_Ceremony.cpp
@@ -1,4 +1,4 @@
-// 28 Feb 2026
+// 3 March 2026
 #include <bits/stdc++.h>
 using ll = long long;
 using lld = long double;
@@ -11,6 +11,11 @@ void solve() {
     ll n, x, y;
     char a, b;
     std::cin >> n >> a >> x >> b >> y;
+
+    if (a == b) {
+        std::cout << std::abs(x - y) << "\n";
+        return;
+    }
     std::cout << std::min((x + std::abs(int(a) - int(b)) + y), std::abs(x - (n + 1)) + std::abs(int(a) - int(b)) + std::abs(y - (n + 1))) << "\n";
 }
 
