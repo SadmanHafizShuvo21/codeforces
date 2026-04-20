85e40f712967901d4e08b56ab73ec1f647917328
SadmanHafiz21 <ug2102021@cse.pstu.ac.bd>
2026-04-20 15:36:25 +0600

    codeforces

---
+++ C_Aziza_Supermarket_Heist.cpp
@@ -0,0 +1,36 @@
+// 20 April 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+const ll mod = 1e6 + 3;
+
+void solve() {
+    ll n;
+    std::cin >> n;
+
+    if (n == 1) {
+        std::cout << 1 << "\n";
+        return;
+    }
+    
+    for (int i = 0; i < n - 2; i++) {
+        std::cout << 1 << " ";
+    }
+    std::cout << 2 << " " << n << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    // solve();
+}
