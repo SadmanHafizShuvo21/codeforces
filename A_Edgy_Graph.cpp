8629bb060fdcd88d6c3ecd05f06a7589b6b9282d
Sadman Hafiz Shuvo <ug2102021@cse.pstu.ac.bd>
2026-04-30 20:53:24 +0600

    codeforces

---
+++ A_Edgy_Graph.cpp
@@ -0,0 +1,45 @@
+// 29 April 2026
+#include <bits/stdc++.h>
+using ll = long long;
+using lld = long double;
+using llx = __int128;
+const ll inf = 1e18;
+const ll N = 2e5 + 7;
+const ll error = 1e-6;
+
+void solve() {
+    ll n, m;
+    std::cin >> n >> m;
+    std::vector<ll> a(n + 1, inf);
+    std::vector<std::tuple<ll, ll, ll>> edges(m);
+    for (int i = 0; i < m; i++) {
+        ll u, v, w;
+        std::cin >> u >> v >> w;
+        edges[i] = {u, v, w};
+        a[u] = std::min(a[u], w);
+        a[v] = std::min(a[v], w);
+    }
+
+    for (auto &[u, v, w] : edges) {
+        if (std::max(a[u], a[v]) != w) {
+            std::cout << -1 << '\n';
+            return;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        a[i] = (a[i] == inf ? 1 : a[i]);
+        std::cout << a[i] << " \n"[i == n];
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    // solve();
+}
